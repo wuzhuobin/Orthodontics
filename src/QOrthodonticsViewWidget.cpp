e38f8ac1b816d7253327724b5dc483a07e31a1dc
WU <zhuobin.wu@philips.com>
2022-12-21 09:55:14 +0800

    Create QOrthodonticsViewWidget

---
+++ src/QOrthodonticsViewWidget.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file QOrthodonticsViewWidget.cpp
+ * @author wuzhuobin (jiejin2022@163.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-21
+ *
+ * @copyright copyright (c) 2022
+ *
+ */
+
+#include "QOrthodonticsViewWidget.hpp"
---
+++ src/QOrthodonticsViewWidget.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file QOrthodonticsViewWidget.hpp
+ * @author wuzhuobin (jiejin2022@163.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-21
+ *
+ * @copyright copyright (c) 2022
+ *
+ */
+
+// vtk
+#include <QVTKOpenGLNativeWidget.h>
+
+class QOrthodonticsViewWidget : public QVTKOpenGLNativeWidget {
+  Q_OBJECT
+};
